728ca9c7fd18e3a417a38631eeb8ca3f861c52db
Erik Scheffers <eriks5@users.noreply.github.com>
2025-07-05 16:50:17 +0200

    Optimize IEEE drives

--- rtl/ieee_drive/ieeedrv_step.sv
+++ rtl/ieee_drive/ieeedrv_step.sv
@@ -14,59 +14,118 @@
  */
 
 module ieeedrv_step (
-   input            clk_sys,
-   input            reset,
+	input        clk_sys,
+	input        reset,
+	input        ce,
 
-	input            drv_type,
+	input        drv_type,
 
-   input            we,
+	input        mounted,
+	input        selected,
+	input        active,
+	input        changing,
 
-   input            img_mounted,
-   input            act,
+	input        mtr,
+	input  [1:0] stp,
+	input        we,
+	input        rw,
+	input        hd,
 
-   input            mtr,
-   input      [1:0] stp,
-
-   output reg       save_track,
-   output     [6:0] track
+	output reg   save_track,
+	output [7:0] track,
+	output       track_changing
 );
 
 // For 4040 "6530-34 RIOT DOS 2" part #901466-04 
 // and 8520 "6530-47 RIOT DOS 2.7 Micropolis" part #901885-04
 // (some other RIOT versions for other drive types use different step motor control signals)
 
-wire [8:0] MAX_HTRACK = 9'(drv_type ? 42*2 : 76*4);
-wire [8:0] DIR_HTRACK = 9'(drv_type ? 17*2 : 38*4);
+// Track constants
+localparam SIDE0_START = 1;
+localparam SIDE1_START = 78;
+wire [8:0] MAX_HTRACK = 9'(drv_type ? 42*2 : 76*4);  // Max half/quarter track number
+wire [8:0] DIR_HTRACK = 9'(drv_type ? 17*2 : 38*4);  // Directory half/quarter track number
+
+// `ce` pulses to wait after drive stops writing before flushing buffer if controller is idle
+localparam SAVE_DELAY = 8_000_000;  // 0.5 seconds = 2.5 rotations at 300 RPM
+
+// max `ce` pulses between stepper pulses
+wire [18:0] CHANGE_DELAY = 19'(drv_type ? 'h4_0000 : 'h2_0000);
+
+// max `ce` pulses between random `hd` signal changes
+localparam HD_CHANGE_DELAY = 1023;
+
+assign track_changing = |change_cnt;
+assign track = track_changing ? track_r 
+										: (drv_type ? 8'(htrack[7:1] + SIDE0_START)
+														: 8'(htrack[8:2] + (hd ? SIDE1_START : SIDE0_START)));
 
 reg  [8:0] htrack;
-
-assign track = drv_type ? htrack[7:1] :  htrack[8:2];
+reg  [7:0] track_r;
+reg [18:0] change_cnt;
 
 always @(posedge clk_sys) begin
-	reg       track_modified;
-	reg [1:0] move, stp_old;
+	reg [22:0] save_cnt;
+	reg  [9:0] hd_cnt;
+	reg        track_modified;
+	reg  [1:0] move, stp_old;
+	reg        hd_old, rw_old;
 
-	stp_old <= stp;
-	move <= stp - stp_old;
+	track_r  <= track;
+	stp_old  <= stp;
+	move     <= stp - stp_old;
 
-	if (we)          track_modified <= 1;
-	if (img_mounted) track_modified <= 0;
+	if (reset || selected) begin
+		hd_old <= hd;
+		rw_old <= rw;
+	end
 
-	if (reset) begin
+	if (change_cnt && ce)
+		change_cnt <= change_cnt - 1'b1;
+
+	if (save_cnt && ce)
+		save_cnt <= save_cnt - 1'b1;
+
+	if (hd_cnt && ce)
+		hd_cnt <= hd_cnt - 1'b1;
+
+	if (reset || mounted) begin
 		htrack <= DIR_HTRACK;
 		track_modified <= 0;
+		change_cnt <= 0;
 	end
-   else begin
-		if (mtr && move[0]) begin
-			if (!move[1] && htrack < MAX_HTRACK) htrack <= htrack + 1'b1;
-			if ( move[1] && htrack > 0         ) htrack <= htrack - 1'b1;
-			if (track_modified) save_track <= ~save_track;
-			track_modified <= 0;
+	else begin
+		if (move[0]) begin
+			if (!move[1] && htrack < MAX_HTRACK) begin
+				htrack     <= htrack + 1'b1;
+				change_cnt <= CHANGE_DELAY;
+			end
+			if (move[1] && htrack > 0) begin
+				htrack     <= htrack - 1'b1;
+				change_cnt <= CHANGE_DELAY;
+			end
 		end
 
-		if (track_modified && !act) begin // stopping activity or changing drives
-			save_track <= ~save_track;
+		if (selected) begin
+			if (we)
+				track_modified <= 1;
+
+			if (hd == hd_old)
+				hd_cnt <= 10'(HD_CHANGE_DELAY);
+			else
+				change_cnt <= CHANGE_DELAY;
+
+			if (rw_old && !rw)
+				change_cnt <= 0;
+
+			if (!track_modified || we)
+				save_cnt <= 23'(SAVE_DELAY);
+		end
+
+		if (track_modified && (changing || move[0] || (selected && (!mtr || !hd_cnt || !save_cnt)))) begin
 			track_modified <= 0;
+			save_track     <= ~save_track;
+			save_cnt       <= 23'(SAVE_DELAY);
 		end
 	end
 end
